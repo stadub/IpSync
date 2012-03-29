e699d85deec66af2f6c97c312b32c4706d01bfaf
root <root@sd-deb.itransition.corp>
2012-03-28 20:11:17 -0400

    [+] Added configuration file [+] Added cron execution file

---
+++ CronExec.sh
@@ -0,0 +1,1 @@
+ /usr/bin/GetIp.sh 'eth0' | /usr/bin/IPSaveBox.sh /root/Dropbox/Settings/IPs
---
+++ configure.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+ln -s $PWD/IPSaveBox.sh /usr/bin/IPSaveBox.sh
+ln -s /root/Dropbox/Projects/Linux/Scripts/GetIp.sh  /usr/bin/GetIp.sh
